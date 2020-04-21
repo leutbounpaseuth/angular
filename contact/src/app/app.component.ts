ae65ad296aec50cbb746857bde003102e4f5d63a
leutbounpaseuth <leut.bounpaseuth@gmail.com>
2020-04-21 08:23:28 +0200

    set isLogged

--- contact/src/app/app.component.ts
+++ contact/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'contact';
+  title : string = 'contact';
+  isLogged : boolean = false;
 }
